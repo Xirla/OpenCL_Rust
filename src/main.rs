c5736344fccdab9ae4b786b19fac32e3f0e74d20
Xirla <83463027+Xirla@users.noreply.github.com>
2021-11-08 18:48:50 +0900

    add comment
    add comment

--- src/main.rs
+++ src/main.rs
@@ -81,6 +81,7 @@ fn main() {
     .enqueue_nd_range(&command_queue)
     .expect("Failed to enqueue kernel execution.");
 
+  // read buffer
   command_queue.enqueue_read_buffer(&mut c_buffer, opencl3::types::CL_TRUE, 0, &mut c[..], &[kernel_event.get()]).expect(
     "Failed to read buffer c."
   );
